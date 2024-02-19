9e8125f95d49b0400846e26f68a2b5fbaf5e60b1
Manish kalwani <149008521+Manish25725@users.noreply.github.com>
2024-02-20 00:25:29 +0530

    solved assignment are added

---
+++ 1-assignment/Ques2.cpp
@@ -0,0 +1,9 @@
+//Ques: Print 10 using 2 positive numbers less than 6  in C++ as output.
+#include<iostream>
+using namespace std;
+int main(){
+    int n1=5;
+    int n2=5;
+    cout<<n1+n2;
+    return 0;
+}
---
+++ 1-assignment/Ques5.cpp
@@ -0,0 +1,11 @@
+//Ques: WAP to find the circumference of a circle with radius 10 in C++.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int rad=10;
+    float pi=3.14;
+    float cir=2*pi*rad;
+    cout<<"The circumference of circle is "<<cir;
+    return 0;
+}
